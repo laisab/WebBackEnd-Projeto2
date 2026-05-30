9614dbdc3a1eef4a9cfe3aad92b285bad37f55d8
laisab <laisaparecidab@gmail.com>
2026-05-30 20:11:30 -0300

    Tentativa de resolver as rotas

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const http = require('http'), express = require('express'), path = require('path'), session = require('express-session'), app = express();
+const http = require('http'), express = require('express'), session = require('express-session'), app = express();
 const Cliente = require('./model/Cliente'), Vendedor = require('./model/Vendedor'), Produto = require('./model/Produto');
+const conectarDB = require('./db');
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
 
 // Middleware que implementa a session
 app.use(session({
@@ -15,6 +19,50 @@ app.get('/teste', (req, res) => {
 });
 
 // Rotas para Cliente
+/*app.get('/cliente/cadastro', (req, res) => {
+    res.end('Cadastro de Cliente');
+});*/
 
+app.post('/cliente/cadastro', async (req, res) => {
+    try{
+        const {nome, cpf, email, senha, endereco} = req.body;
+        const cliente = new Cliente(nome, cpf, email, senha, endereco);
 
-http.createServer(app).listen(8000);
+        await cliente.validar();
+        await cliente.inserirDB();
+
+        res.status(201).json({mensagem: "Cliente cadastrado com sucesso!"});
+    }catch(err){
+        res.status(403).json({erro: "Erro ao cadastrar cliente"});
+        Cliente.logError(err);
+    }
+});
+
+app.post('/cliente/login', async (req, res) => {
+    try{
+        const cpf = req.body.cpf, senha = req.body.senha;
+        const cpfCadastrado = await Cliente.pesquisarCpf(cpf);
+
+        if(!cpfCadastrado){
+            return res.status(401).json({message : "Cliente não encontrado, tente novamente!"});
+        }
+
+        if(cliente.senha !== senha){
+            return res.status(401).json({message : "Senha incorreta, tente novamente!"});
+        }
+
+        req.session.cliente = {
+            nome: cliente.nome,
+            cpf: cliente.cpf,
+            email: cliente.email,
+            endereco: cliente.endereco
+        };
+
+        res.status(201).json({message : "Cliente logado com sucesso!"});
+    }catch(err){
+        res.status(403).json({erro: "O cliente não forneceu as credenciais corretas para acessar o recurso"});
+        Cliente.logError(err);
+    }
+});
+
+app.listen(8000);
